9f14ae91901d49741ea05387873ac1f0698ce2b7
Andres Merizalde <andymerizalde@me.com>
2013-07-11 22:37:35 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
  app.get('/', function(request, response) {
  var fs = require('fs');
  fs.readFileSync("index.html", "utf-8");
-response.send(resultOfs.readFileSync);
+response.send(resultOfreadFileSync);
   });
 
 var port = process.env.PORT || 5000;
