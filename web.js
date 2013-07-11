acca79266899aa0ce0f31b90430dbdd5af18e4ed
Andres Merizalde <andymerizalde@me.com>
2013-07-11 22:35:47 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
  app.get('/', function(request, response) {
-  fs.readFileSync("index.html", "utf-8");
+ var fs = require('fs');
+ fs.readFileSync("index.html", "utf-8");
 response.send(resultOfs.readFileSync);
   });
 
